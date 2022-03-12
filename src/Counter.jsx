b1e250f84dd85cfa4eb4f7e672bc9c57bde65334
Jason Serafica <seraficaj@gmail.com>
2022-03-12 11:17:12 -0800

    add user state

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 
 function Counter() {
     const [count, setCount] = useState(0);
+    const [user, setUser] = useState({
+        name: "Jason",
+        favFood: 'macaroni'
+    });
     const handleIncreaseCount = () => {
         setCount(count + 1);
     };
@@ -18,6 +22,25 @@ function Counter() {
             <button onClick={handleIncreaseCount}>+</button>
             <button onClick={handleResetCount}>Reset</button>
             <button onClick={handleDecreaseCount}>-</button>
+            <h2>the current user is: {user.name}</h2>
+            <h2>their favFood is: {user.favFood}</h2>
+            <form>
+                <label htmlFor="name-input">Enter your name</label>
+                <input
+                    type="text"
+                    id="name-input"
+                    placeholder="enter username"
+                    onChange={e => setUser({...user, name: e.target.value})}
+                    value = {user.name}
+                />
+                  <input
+                    type="text"
+                    id="food-input"
+                    placeholder="enter fave food"
+                    onChange={e => setUser({...user, favFood: e.target.value})}
+                    value = {user.favFood}
+                />
+            </form>
         </div>
     );
 }
